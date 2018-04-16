dfef06fca82469a1b81015f0e3037ce95d496cec
0xA01 <cryptoape@icloud.com>
2018-04-16 14:55:09 +0300

    Disable zICH Automint be default

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -44,6 +44,7 @@ public:
         DatabaseCache,       // int
         SpendZeroConfChange, // bool
         ZeromintEnable,      // bool
+        ZeromintDisable,     // bool
         ZeromintPercentage,  // int
         ZeromintPrefDenom,   // int
         HideZeroBalances,    // bool
